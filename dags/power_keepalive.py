fc1429e11a3d6f6ccdc5dc4f10d2a904d8527e60
Артур Камалов <artkmlv@yandex.ru>
2025-11-23 18:38:02 +0300

    Add DAG: power_keepalive.py

---
+++ dags/power_keepalive.py
@@ -0,0 +1,35 @@
+"""
+Обращение к сайту на streamlit для пинга для поддержания его работоспособности
+Запуск каждые 2 часа
+"""
+
+import requests
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+URL = "https://power-interactive-plot.streamlit.app/"
+
+
+def ping_site():
+    resp = requests.get(URL, timeout=60)
+    resp.raise_for_status()
+
+
+with DAG(
+    dag_id="power_keepalive",
+    start_date=datetime(2025, 11, 1),
+    schedule="0 */2 * * *",
+    catchup=False,
+    max_active_runs=1,
+    default_args=dict(retries=3),
+    tags=["streamlit"],
+) as dag:
+
+    dag.doc_md = __doc__
+
+    PythonOperator(
+        task_id="ping",
+        python_callable=ping_site,
+    )
